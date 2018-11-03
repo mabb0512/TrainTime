2541bc671e39e60d95b9d5e7da341c77e6e5dbde
mabb0512 <mariana.bravo@gmail.com>
2018-11-03 14:31:10 -0400

    check for empty fields

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,39 +13,55 @@ $("#train-submit").on("click", function(event) {
 
     event.preventDefault();
 
-    //get all trains in database
-    database.ref().on("value", function(snapshot) {
-        var result = snapshot.toJSON();
-        if (!result == undefined)
-            savedTrains = Object.keys(result).length;
-    });
-
-    //get user input data
-    var id = savedTrains + 1;
     var name = $("#train-name").val().trim();
     var destination = $("#train-dest").val().trim();
     var time = $("#train-time").val().trim();
     var frequency = $("#train-freq").val().trim();
 
-    // Creates object with train data
-    var newTrain = {
-        trainId: id,
-        trainName: name,
-        trainDest: destination,
-        trainTime: time,
-        trainFreq: frequency
-    };
+    if (name.length == 0 || destination.length == 0 || time.length == 0 || frequency.length == 0) {
 
-    //add train to db
-    database.ref().push(newTrain);
+        alert ("Fields cannot be empty");
+    }
 
-    alert("Train added successfully");
+    else {
 
-    // Clears all of the text-boxes
-    $("#train-name").val("");
-    $("#train-dest").val("");
-    $("#train-time").val("");
-    $("#train-freq").val("");
+                //get all trains in database
+                database.ref().on("value", function(snapshot) {
+
+                    var result = snapshot.toJSON();
+                    if (result != null)
+                        savedTrains = Object.keys(result).length;
+                });
+        
+                //get user input data
+                var id = savedTrains + 1;
+                var name = $("#train-name").val().trim();
+                var destination = $("#train-dest").val().trim();
+                var time = $("#train-time").val().trim();
+                var frequency = $("#train-freq").val().trim();
+        
+                // Creates object with train data
+                var newTrain = {
+                    trainId: id,
+                    trainName: name,
+                    trainDest: destination,
+                    trainTime: time,
+                    trainFreq: frequency
+                };
+        
+                //add train to db
+                database.ref().push(newTrain);
+        
+                alert("Train added successfully");
+        
+                // Clears all of the text-boxes
+                $("#train-name").val("");
+                $("#train-dest").val("");
+                $("#train-time").val("");
+                $("#train-freq").val("");
+    }
+
+
 
 });
 
@@ -58,7 +74,6 @@ database.ref().on("child_added", function(childSnapshot) {
     var frequency = childSnapshot.val().trainFreq;
 
     var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-    var currentTime = moment();
     var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
     var timeApart = diffTime % frequency;
     var minutesAway = frequency - timeApart;
